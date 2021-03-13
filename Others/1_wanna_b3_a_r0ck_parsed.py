6ce80e119b0737a3a3748644e47c7a0941188aa9
KDChang <KiD21606@users.noreply.github.com>
2021-03-13 12:24:08 +0800

    Create 1_wanna_b3_a_r0ck_parsed.py

---
+++ Others/1_wanna_b3_a_r0ck_parsed.py
@@ -0,0 +1,32 @@
+rocknroll = 5                           # Rocknroll is right
+silence = 5                             # Silence is wrong
+a_guitar = 10                           # A guitar is a six-string
+tommy = 44                              # Tommy's been down
+music = 170                             # Music is a billboard-burning razzmatazz
+the_music = input()                     # Listen to     the music
+if the_music==a_guitar:                 # If the music is a guitar
+    print("Keep on rocking!")           # Say "Keep on rocking!"
+    the_rhythm = input()                # Listen to the rhythm
+    if rhythm-music==0:                 # If the rhythm without Music is nothing
+        tommy = 66                      # Tommy is rockin guitar
+        print(tommy)                    # Shout Tommy!
+        music = 79                      # Music is amazing sensation
+        jamming = 78                    # Jamming is awesome presence
+        print(music)                    # Scream Music!
+        print(jamming)                  # Scream Jamming!
+        tommy = 74                      # Tommy is playing rock
+        print(tommy)                    # Scream Tommy!
+        tommy = 79                      # They are dazzled audiences
+        print(tommy)                    # Shout it!
+        rock = 86                       # Rock is electric heaven
+        print(rock)                     # Scream it!
+        tommy = 73                      # Tommy is jukebox god
+        print(tommy)                    # Say it!
+        tommy -= 1                      # Break it down
+        print("Bring on the rock!")     # Shout "Bring on the rock!"
+    else:
+        print("That ain't it, Chief")   # Else Whisper "That ain't it, Chief"
+        Break                           # Break it down
+
+=> 66, 79, 78, 74, 79, 86, 73
+=> BONJOVI
